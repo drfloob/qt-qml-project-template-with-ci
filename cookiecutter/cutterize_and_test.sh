3e60714a0ac2039c22050221351bc532e868f838
AJ Heller <aj@drfloob.com>
2020-04-15 21:13:47 -0700

    attempt to push a branch to origin from github workflows

--- cookiecutter/cutterize_and_test.sh
+++ cookiecutter/cutterize_and_test.sh
@@ -10,4 +10,15 @@
 
 set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
-echo "HELLO WORLD"
+git config --global user.name "Workflow Worker"
+git config --global user.email "nobody@219design.com"
+
+cd $GITHUB_WORKSPACE
+echo "HELLO WORLD" >> README.md
+git add .
+git checkout -b qt_cookiecutter
+git commit -m "cookiecutterized invocation($INVOCATION_ID)"
+
+remote_repo="https://${COOKIECUTTER_PUSH_USER}:${COOKIECUTTER_PUSH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git push "${remote_repo}" HEAD:test --force 
