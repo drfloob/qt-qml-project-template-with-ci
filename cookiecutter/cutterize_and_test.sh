d5c0501b19cb11db189aaa49393de999aa712394
AJ Heller <aj@drfloob.com>
2020-04-15 21:03:55 -0700

    git workflow: cookiecutter hello world

---
+++ cookiecutter/cutterize_and_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2020, 219 Design, LLC
+# See LICENSE.txt
+#
+# https://www.219design.com
+# Software | Electrical | Mechanical | Product Design
+#
+
+set -Eeuxo pipefail # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+
+echo "HELLO WORLD"
